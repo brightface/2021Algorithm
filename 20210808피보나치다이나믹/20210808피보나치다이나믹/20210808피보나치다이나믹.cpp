a1a443912388b41cc31872e8f51a36c62b0ac233
brightface <brightface@hanmail.net>
2021-08-10 20:55:54 +0900

    피보나치다이나믹
    0이면 갱신안되엇다는 뜻

---
+++ 20210808피보나치다이나믹/20210808피보나치다이나믹/20210808피보나치다이나믹.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int fiboarr[50] = { 0,1, };
+int fibo(int N)
+{
+    if (N == 0 || N == 1)
+        return fiboarr[N];
+    else if (fiboarr[N] == 0) //dp의 핵심. 0이면 갱신이 안되었다는 뜻. 함수를 돌려 갱신하자.
+        fiboarr[N] = fibo(N - 1) + fibo(N - 2);
+    return fiboarr[N];
+}
+int main() {
+    int T;
+    cin >> T;
+    int tmp;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> tmp;
+        if (tmp == 0)
+            cout << "1 0" << '\n';
+        else
+            cout << fibo(tmp - 1) << ' ' << fibo(tmp) << '\n';
+    }
+}
